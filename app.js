d6dc043236e8a190613943e2a995367a3d8ec72d
Tenzin Nargee <tenzin.n@Tenzins-MacBook-Pro.local>
2023-10-02 16:25:55 -0400

    First implementation of clicking response for boards

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+// Initialize a variable to keep track of the current player
+let currentPlayer = 'X';
+
+// Add a click event listener to each cell
+document.querySelectorAll('.cell').forEach(cell => {
+    cell.addEventListener('click', handleCellClick);
+});
+
+// Function to handle the cell click event
+function handleCellClick(event) {
+    const cell = event.target;
+
+    // Check if the cell is already filled
+    if (cell.textContent !== '') {
+        return;
+    }
+
+    // Fill the cell with the current player's symbol
+    cell.textContent = currentPlayer;
+
+    // Switch to the other player
+    currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+}
 
